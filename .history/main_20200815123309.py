b0c49f9f4d3bb7a4d9ec617e0d7299a5ad6b7bb9
SarahAbdullah <dev.abdullah0615@gmail.com>
2020-08-15 12:34:42 -0400

    New updates

--- .history/main_20200815123309.py
+++ .history/main_20200815123309.py
@@ -1,32 +1,24 @@
+import os
+import sys
+
 class HtmlDocument:
-    def __init__(self):
+    def__init__(self):
         pass
-
-
 
 class HtmlManager:
-    def __init__(self):
+    def__init__(self):
         pass
-        
+
     def writehtml(self):
         samplehtml = open('helloworld.html','w')
-        
         message = """<html><head></head>
-        <body><p>Hello World!</p></body>
-        </html>"""
-        
+        <body><p>Hello WOrld!</></body><html>"""
         samplehtml.write(message)
         samplehtml.close()
-        
-        
-        
-            
-    
-
-
-class AWSManager:
+class AWSManager:  
     def __init__(self):
-        pass
+        pass      
+
 
 
 s1 = HtmlManager()
